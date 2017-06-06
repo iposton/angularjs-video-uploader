b264acab354a344162ad1136c33819a542a3426d
iposton <ianposton@sbcglobal.net>
2017-06-05 21:48:37 -0700

    refactor video-uploader ctrl

--- client/app/app.js
+++ client/app/app.js
@@ -1,9 +1,6 @@
 (function(angular) {
     'use strict';
 
-    var url = 'https://upload.wistia.com/?access_token='+API_TOKEN;
-    var mediaUrl = 'https://api.wistia.com/v1/medias.json';
-    var apiPassword = API_TOKEN;
     angular.module('app', ['blueimp.fileupload'])
         .config([
             '$httpProvider', 'fileUploadProvider',
@@ -14,115 +11,133 @@
                     '/cors/result.html?%s'
                 );
 
-                  angular.extend(fileUploadProvider.defaults, {
-                        // Enable image resizing, except for Android and Opera,
-                        // which actually support image resizing, but fail to
-                        // send Blob objects via XHR requests:
-                        disableImageResize: /Android(?!.*Chrome)|Opera/
-                            .test(window.navigator.userAgent),
-                        maxFileSize: 40000000,
-                        acceptFileTypes: /(\.|\/)(mp4|mov|MOV)$/i
-                    });
+                angular.extend(fileUploadProvider.defaults, {
+                    // Enable image resizing, except for Android and Opera,
+                    // which actually support image resizing, but fail to
+                    // send Blob objects via XHR requests:
+                    disableImageResize: /Android(?!.*Chrome)|Opera/
+                        .test(window.navigator.userAgent),
+                    maxFileSize: 40000000,
+                    acceptFileTypes: /(\.|\/)(mp4|mov|MOV)$/i
+                });
             }
         ])
         .component('videoUploader', {
             transclude: true,
-            controller: function videoUploaderCtrl($scope, $http, $window, $filter, $timeout) {
-                var status = null;
-                //Watch for video status from Wistia API
-                $scope.$watch('status', function(newVal, oldVal) {
-
-                  console.log(newVal, 'new val');
-                  if(newVal === 'queued') {
-                    $scope.queued = "Video is being queued by Wistia..."
-                  }
-
-                }, true)
-                
-                $scope.getFile = function(file) {
-                     console.log(file, 'the master file upload');
-                }
-                
-
-                $scope.options = {
-                    url: url,
-                    done: function(e, data) {
-                        console.log(data, 'done data');
-                        //After upload get video from Wistia API 
-                        getVideos();
-                        //After upload is done clear out the files
-                        var scope = data.scope;
-                        scope.clear(data.files);
-                       
-
-                    }
-                };
-
-               $scope.loadingFiles = true;
-               console.log($scope.loadingFiles, 'loading...');
-        
-
-
-                var getVideos = function() {
-                   //$scope.progress = true;
-                   console.log($scope.progress, 'progress...');
-                    $http({
-                        method: 'get',
-                        url: mediaUrl,
-                        params: {
-                            api_password: apiPassword
-                        }
-                    }).then(function(response) {
-                        console.log(response.data, 'res');
-                        if (response.data.length === 0) {
-                          $scope.messagetext = true;
-                          return $scope.message = "There are no videos to show."
-                        } else {
-                          $scope.messagetext = false;
-                          $scope.embedArray = response.data;
-                          $scope.loadingFiles = false;
-                          $scope.hashed_id = response.data[0].hashed_id;
-                          
-                          //Get video status for all videos in the array
-                          angular.forEach($scope.embedArray, function (item) {
-                                 $scope.status = item.status;
-                            
-                          })
-                                               
-                        }
-                      
-
-                    }).catch(function(error) {
-                       $scope.loadingFiles = false;
-                        //If error throw error
-                        console.error("Error with GET request", error);
-                    })
-
-                }
-
-                $scope.destroy = function (id) {
-                   $http({
-                        method: 'delete',
-                        url: 'https://api.wistia.com/v1/medias/'+id+'.json',
-                        params: {
-                            api_password: apiPassword
-                        }
-                    }).then(function(response) {
-                      $scope.embedArray = [];
-                      getVideos();
-                        console.log(response, 'res delete');
-
-                    }).catch(function(error) {
-                      
-                        //If error throw error
-                        console.error("Error with DELETE request", error);
-                    })
-                }
-                //Get videos from Wistia API right away. 
-                getVideos();
-
-            },
+            controller: videoUploaderCtrl,
+            controllerAs: 'vm',
             templateUrl: 'app/video-uploader.html'
+
         })
- 
+
+    function videoUploaderCtrl($scope, $http, $window, $filter, $timeout) {
+
+        var self = this;
+
+        //Wistia-upload-api config vars
+        var wistiaApi = {
+            url: 'https://upload.wistia.com/?access_token=' + API_TOKEN,
+            mediaUrl: 'https://api.wistia.com/v1/medias.json',
+            apiPassword: API_TOKEN
+        }
+        var status = null;
+
+        //Define functions
+        self.getFile = getFile;
+        self.getVideo = getVideo;
+        self.destroyVideo = destroyVideo;
+
+        //Watch for video status from Wistia API
+        $scope.$watch('status', function(newVal, oldVal) {
+
+            console.log(newVal, 'new val');
+            if (newVal === 'queued') {
+                self.queued = "Video is being queued by Wistia..."
+            }
+
+        }, true)
+
+        function getFile(file) {
+            console.log(file, 'the master file upload');
+        }
+
+
+        self.options = {
+            url: wistiaApi.url,
+            done: function(e, data) {
+                console.log(data, 'done data');
+                //After upload get video from Wistia API 
+                self.getVideo();
+                //After upload is done clear out the files
+                var scope = data.scope;
+                scope.clear(data.files);
+            }
+        };
+
+        self.loadingFiles = true;
+        console.log($scope.loadingFiles, 'loading...');
+
+        function getVideo() {
+            $http({
+                method: 'get',
+                url: wistiaApi.mediaUrl,
+                params: {
+                    api_password: wistiaApi.apiPassword
+                }
+            }).then(function(response) {
+                console.log(response.data, 'res');
+                if (response.data.length === 0) {
+                    self.messagetext = true;
+                    return self.message = "No videos to show."
+                } else {
+                    self.messagetext = false;
+                    self.videoArray = response.data;
+                    self.loadingFiles = false;
+                    self.hashed_id = response.data[0].hashed_id;
+
+                    //Get video status for all videos in the array
+                    angular.forEach($scope.embedArray, function(item) {
+                        $scope.status = item.status;
+
+                    })
+
+                }
+
+
+            }).catch(function(error) {
+                self.loadingFiles = false;
+                //If error throw error
+                console.error("Error with GET request", error);
+            })
+
+        }
+
+        function destroyVideo(id) {
+            var deleteVideo = $window.confirm('Are you sure you want to delete the Video?');
+            if (deleteVideo) {
+                $http({
+                    method: 'delete',
+                    url: 'https://api.wistia.com/v1/medias/' + id + '.json',
+                    params: {
+                        api_password: wistiaApi.apiPassword
+                    }
+                }).then(function(response) {
+                    //After deleting video clear view and get videos
+                    self.videoArray = [];
+                    self.getVideo();
+                    console.log(response, 'res delete');
+                }).catch(function(error) {
+                    //If error throw error
+                    console.error("Error with DELETE request", error);
+                })
+
+            }
+        }
+
+        //Get video files from Wistia API right away. 
+        self.getVideo();
+
+
+    }
+
 })(window.angular);
